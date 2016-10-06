2df500e0afaa12a4bfb7e3ef6b652e7b78ace000
Brandon Powell <bpowell29a@gmail.com>
2016-10-06 18:45:51 -0400

    Icon added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class TabLink extends Component {
+  switchTab(id) {
+      chrome.tabs.update(id, {active: true});
+  }
+
+  render() {
+      return (
+          <div>
+            <img width="30" height="30" src={this.props.tab.favIconUrl} />
+            <a onClick={() => this.switchTab(this.props.tab.id)}>{this.props.tab.title}</a>
+            </div>
+      );
+  }
+}
+
 class App extends Component {
   constructor() {
       super();
@@ -10,16 +25,12 @@ class App extends Component {
       };
   }
 
-  switchTab(id) {
-      chrome.tabs.update(id, {active: true});
-  }
-
   getTabs() {
     chrome.tabs.query({}, function(tabs) {
         console.error(chrome.pageAction);
       let content = []
       for(let i = 0; i<tabs.length; i++) {
-          content.push(<div><a onClick={() => this.switchTab(tabs[i].id)}>Hi! - {tabs[i].title}</a></div>);
+          content.push(<TabLink tab={tabs[i]} />);
       }
 
       this.setState({content: content});
