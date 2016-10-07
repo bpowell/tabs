476be4b7dee8a91ecd11ebce9bfcdc6b4fc74417
Brandon Powell <bpowell29a@gmail.com>
2016-10-06 22:35:15 -0400

    Removed hash from css output files

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -220,7 +220,7 @@ module.exports = {
       }
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    new ExtractTextPlugin('static/css/[name].[contenthash:8].css')
+    new ExtractTextPlugin('static/css/[name].css')
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
